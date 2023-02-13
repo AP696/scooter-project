79d34ca284837cbb3add0a28dd2b1cd1f40bd925
AP696 <addyphilips66@gmail.com>
2023-02-13 14:08:05 +0000

    complete

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -10,6 +10,32 @@ class ScooterApp {
     };
     this.registeredUsers = {};
   }
+
+  registerUser(username, password, age) {
+    if (!this.registeredUsers[username] && age >= 18) {
+      this.registeredUsers[username] = new User(username,password,age);
+      console.log(`User ${username} registered`);
+      return this.registeredUsers[username];
+    } else if (this.registeredUsers[username]) {
+      console.error("User already registered")
+    } else {
+      console.error("User too young to register");
+    }
+  }
+
+  loginUser(username, password) {
+    if (this.registeredUsers[username]) {
+      if (this.registeredUsers[username].password === password) {
+        this.registeredUsers[username].login(password);
+        console.log(`User ${username} logged in`);
+      } else {
+        console.error("Incorrect password");
+      }
+    } else {
+      console.error("Username or password incorrect")
+    }
+  }
+
 }
 
 module.exports = ScooterApp
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -3,34 +3,63 @@ const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp');
 const { useRef } = require('react');
 
-
 describe("ScooterApp", () => {
     let scooterApp = new ScooterApp();
     describe("registerUser", () => {
         test("should register a user successfully", () => {
-            const user = scooterApp.registerUser("test-username", "test-password", 21);
-            expect(user).toEqual({
-                username: "test-username",
-                password: "test-password",
-                age: 21,
-            })
+            const user = scooterApp.registerUser("test_username", "test_password", 21);
+            expect(user.username).toEqual("test_username");
+            expect(user.password).toEqual("test_password");
+            expect(user.age).toEqual(21);
+            expect(user.loggedIn).toEqual(false);
             expect(scooterApp.registeredUsers).toEqual({
                 test_username: {
-                    username: "test-username",
-                    password: "test-password",
+                    username: "test_username",
+                    password: "test_password",
                     age: 21,
+                    loggedIn: false
                 }
             })
         });
 
         test("should not register a user if username is already taken", () => {
-            
+            scooterApp.registerUser("test_username", "test_password", 21);
+            const user = scooterApp.registerUser("test_username", "test_password", 21);
+            expect(user).toBeUndefined();
+            expect(scooterApp.registeredUsers).toEqual({
+                test_username: {
+                    username: "test_username",
+                    password: "test_password",
+                    age: 21,
+                    loggedIn: false
+                }
+            })
         })
 
         test("should not register a user if they are under 18 years old", () => {
-            const user = scooterApp.registerUser("test-username", "test-password", 17);
+            const scooterApp = new ScooterApp()
+            const user = scooterApp.registerUser("test_username", "test_password", 17);
             expect(user).toBeUndefined();
-            expect(scooterApp.user).toEqual({})
+            expect(Object.keys(scooterApp.registeredUsers)).toHaveLength(0)
+        })
+    })
+
+    describe("loginUser", () => {
+        test("should log in a registered user with correct name and password", () => {
+            scooterApp.registerUser("test_username", "test_password", 21);
+            scooterApp.loginUser("test_username", "test_password");
+            expect(scooterApp.registeredUsers.test_username.loggedIn).toEqual(true);
+        })
+
+        test("should not log in a registered user with the incorrect password", () => {
+            scooterApp.loginUser("test_username", "incorrect_password");
+            expect(scooterApp.registeredUsers.test_username.loggedIn).toEqual(false);
+        });
+
+        test("it should not log in an unregistered user", () => {
+            scooterApp.loginUser('unregistered_username', "test_password");
+            expect(scooterApp.registeredUsers['unregistered_username']).toBeUndefined();
         })
     })
 })
+ 
